1766502f4a9f681ae44562e3c2583ebf8c518200
Javier Sorella <javier.sorella@globant.com>
2017-12-28 13:56:07 -0300

    Tests improvements

--- spec/random_get_spec.rb
+++ spec/random_get_spec.rb
@@ -4,41 +4,54 @@ require 'webmock/rspec'
 RSpec.describe RandomGet do
 
   let(:random) { RandomGet.new "dbb7f816-b792-496e-ad56-990fc2b2bd7b" }
-  let(:expected_response_body) do
-    {
-      jsonrpc: "2.0",
-      result: {
-        random: {
-          data: [6095],
-          completionTime: "2017-12-27 15:19:23Z"
-        },
-        bitsUsed: 13,
-        bitsLeft: 249961,
-        requestsLeft: 997,
-        advisoryDelay: 1440
-      },
-      id: 1
-    }.to_json
+  
+  it 'Has a version number' do
+    expect(RandomGet::VERSION).not_to be nil
   end
+  
+  context 'When component calls to API' do
+    let(:expected_response_body) do
+      {
+        jsonrpc: "2.0",
+        result: {
+          random: {
+            data: [6095],
+            completionTime: "2017-12-27 15:19:23Z"
+          },
+          bitsUsed: 13,
+          bitsLeft: 249961,
+          requestsLeft: 997,
+          advisoryDelay: 1440
+        },
+        id: 1
+      }.to_json
+    end
+    
+    before :each do
+      stub_request(
+        :post, 
+        "https://api.random.org/json-rpc/1/invoke"
+      ).with(
+        body: {
+          params: {
+            apiKey: "dbb7f816-b792-496e-ad56-990fc2b2bd7b", 
+            n: 1, 
+            min: 0, 
+            max: 9999
+          }, 
+          method: "generateIntegers",
+          jsonrpc: "2.0",
+          id: 1
+        }.to_json,
+        headers: {'Content-Type'=>'application/json-rpc'}
+      ).to_return(
+        status: 200, 
+        body: expected_response_body, headers: {}
+      )
+    end
 
-  context 'one' do
-    it 'Has a version number' do
-      expect(RandomGet::VERSION).not_to be nil
+    it 'Get method returns an Integer' do
+      expect(random.get).to be_a_kind_of(Integer)
     end
   end
-
-
-  
-
-  it 'Get method returns an Integer' do
-    stub_request(:post, "https://api.random.org/json-rpc/1/invoke").
-         with(body: "{\"params\":{\"apiKey\":\"dbb7f816-b792-496e-ad56-990fc2b2bd7b\",\"n\":1,\"min\":0,\"max\":9999},\"method\":\"generateIntegers\",\"jsonrpc\":\"2.0\",\"id\":1}",
-              headers: {'Content-Type'=>'application/json-rpc'}).
-         to_return(status: 200, body: expected_response_body, headers: {})
-
-    expect(random.get).to be_a_kind_of(Integer)
-  end
-
-
-
 end
