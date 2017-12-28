1b443c74be034699c03939536a14889edafa8945
gdeoliveira <gdeoliveira@tribpub.com>
2017-12-28 15:26:09 -0300

    WebMock expectation example.

--- lib/random_get.rb
+++ lib/random_get.rb
@@ -1,31 +1,34 @@
 require 'random_get/version'
 require 'net/http'
 require 'json'
-require 'uri'
-
 
 class RandomGet
+  REQUEST_BODY_BASE = {
+    method: "generateIntegers".freeze,
+    jsonrpc: "2.0".freeze,
+    id: 1
+  }.freeze
+  REQUEST_HEADERS = { "Content-Type".freeze => "application/json".freeze }.freeze
+  REQUEST_URI = URI("https://api.random.org/json-rpc/1/invoke".freeze).freeze
 
   def initialize(api_key, min=0, max=9999)
     @api_key = api_key
     @min = min
     @max = max
   end
-  
+
   def get   
-    uri = URI("https://api.random.org/json-rpc/1/invoke")
-    headers = {"Content-Type" => "application/json-rpc"}
-    params = {params: {
-                       apiKey: @api_key,
-                       n: 1,
-                       min: @min,
-                       max: @max
-                    },
-            method: "generateIntegers",
-            jsonrpc: "2.0",
-            id: 1
-            }
-        
+    uri = REQUEST_URI
+    headers = REQUEST_HEADERS
+    params = REQUEST_BODY_BASE.merge({
+      params: {
+        n: 1,
+        apiKey: @api_key,
+        min: @min,
+        max: @max
+      }
+    })
+
     # Create the HTTP objects
     https = Net::HTTP.new(uri.host,uri.port)
     https.use_ssl = true
